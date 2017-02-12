7a086b3dc3ba4a8d49ffd4875b755531622d91d7
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-02-12 16:47:17 +0300

    changed gulpfile and fis

--- application/fis-conf.js
+++ application/fis-conf.js
@@ -10,13 +10,7 @@ fis.config.set('pack', {
     ],
     'pkg/layout.js': [
         'static/js/layout.js',
-        'static/output/macros.js',
-        'static/js/libs/jquery.easing.1.3.min.js',
-        'static/js/jquery.sticky.js',
-        'static/js/libs/jquery.magnific-popup.min.js',
-        'static/js/jquery.ajaxchimp.js',
-        'static/js/jquery.app.js',
-        'static/js/switcher.js'
+        'static/output/macros.js'
     ],
     'pkg/libs.css': [
         'static/css/libs/*.css'
@@ -25,10 +19,19 @@ fis.config.set('pack', {
         'static/css/bootstrap.theme.css',
         'static/css/common.css',
         'static/output/macros.css',
-        'static/css/layout.css',
-        'static/css/style.css',
+        'static/css/layout.css'
+    ],
+    'pkg/style.css': [
         'static/css/magnific-popup.css',
-        'static/css/pe-icon-7-stroke.css',
-        'static/css/colors/default.css'
+        'static/css/style.css',
+        'static/css/colors/default.css',
+    ],
+    'pkg/style.js': [
+        'static/js/libs/jquery.easing.1.3.min.js',
+        'static/js/jquery.sticky.js',
+        'static/js/libs/jquery.magnific-popup.min.js',
+        'static/js/jquery.ajaxchimp.js',
+        'static/js/jquery.app.js',
+        'static/js/switcher.js'
     ]
 });
