3c66889ccc044fda244f77fa7a3eacb943c07c4c
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-02-12 15:09:30 +0300

    Changed production config

--- config/production_sample.py
+++ config/production_sample.py
@@ -6,13 +6,13 @@ class ProductionConfig(Config):
     # App config
     SECRET_KEY = "\xb5\xb3}#\xb7A\xcac\x9d0\xb6\x0f\x80z\x97\x00\x1e\xc0\xb8+\xe9)\xf0}"
     PERMANENT_SESSION_LIFETIME = 3600 * 24 * 7
-    SESSION_COOKIE_NAME = 'global_session'
+    SESSION_COOKIE_NAME = 'flask-global_session'
 
     # Site domain
-    SITE_DOMAIN = "http://www.global.com"
+    SITE_DOMAIN = "ремонт-пк-и-ноутбуков.бел"
 
     # Db config
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/global"
+    SQLALCHEMY_DATABASE_URI = "postgresql://global@localhost/global"
 
     # Sentry
     SENTRY_DSN = ''
