ced91c449b3ab834449899b22072e1f4a338e24f
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-02-12 16:35:20 +0300

    changed gulpfile and fis

--- application/fis-conf.js
+++ application/fis-conf.js
@@ -12,9 +12,9 @@ fis.config.set('pack', {
         'static/js/layout.js',
         'static/output/macros.js',
         'static/js/libs/jquery.easing.1.3.min.js',
-        'static/js/jquery.sticky.js'
-        'static/js/libs/jquery.magnific-popup.min.js'
-        'static/js/jquery.ajaxchimp.js'
+        'static/js/jquery.sticky.js',
+        'static/js/libs/jquery.magnific-popup.min.js',
+        'static/js/jquery.ajaxchimp.js',
         'static/js/jquery.app.js',
         'static/js/switcher.js'
     ],
