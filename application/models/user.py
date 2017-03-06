aaf0d035f84a3f84f9ef024a6c8f4ab9f033cfa1
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-03-07 02:07:18 +0300

    changed socketio file

--- application/models/user.py
+++ application/models/user.py
@@ -33,8 +33,7 @@ class User(Base, UserMixin):
         backref=db.backref('users'))
     active = db.Column(db.Boolean())
     profile = relationship('Profile', 
-        backref='profile_user', 
-        uselist=False)
+        backref='profile_user')
     confirmed_at = db.Column(db.DateTime())
     last_login_at = db.Column(db.DateTime())
     current_login_at = db.Column(db.DateTime())
